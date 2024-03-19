97aa1bd9be96ea04efe75583ebf55dcf27522824
christianjjc <christianjjc@gmail.com>
2024-03-19 10:20:21 -0500

    primer commit en calc

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StatusBar, View} from 'react-native';
+import {styles} from './config/theme/app-theme';
+import {CalculatorScreen} from './presentation/screens/CalculatorScreen';
+
+function App() {
+  return (
+    <View style={styles.background}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
+      <CalculatorScreen />
+    </View>
+  );
+}
+
+export default App;
